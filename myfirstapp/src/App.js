fcbdd89af2406cbd42957d22a1872d1dfceb0305
traii9195 <traii9195@gmail.com>
2023-04-06 12:42:58 +0100

    react-project

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Formelo from './omar/Formelo';
+import Headerr from './omar/Headerr';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <>
+     < Headerr/>
+    < Formelo/>
+   
+
+    </>
+  );
+}
+
+export default App;
