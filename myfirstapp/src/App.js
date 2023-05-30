f1924100d273c188303fba3cb39891f0176b903b
traii9195 <124260203+traii9195@users.noreply.github.com>
2023-05-30 14:38:47 +0100

    Update App.js

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,12 +1,16 @@
+{/* importation de nos composants  */}
 
-import './App.css';
 import Formelo from './omar/Formelo';
 import Headerr from './omar/Headerr';
+{/* l importation de notre sylesheet bootstrap */}
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
+    {/* l appele de nos composants */}
+
     <>
      < Headerr/>
     < Formelo/>
